29510002d96a2ecd92ec219aca13edcfe52b145c
cphsiung <chunpeihs@gmail.com>
2020-07-04 22:13:17 -0400

    Added update route

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
-var bodyParser = require("body-parser"),
-	mongoose = require("mongoose"),
-	express = require("express"),
-	app = express()
+var bodyParser = 		require("body-parser"),
+	methodOverride = 	require("method-override"),
+	mongoose = 			require("mongoose"),
+	express = 			require("express"),
+	app = 				express()
 
 // APP CONFIG
 mongoose.connect("mongodb://localhost/blog_app");
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(methodOverride("_method"));
 
 // MONGOOSE/MODEL CONFIG
 var blogSchema = new mongoose.Schema({
@@ -62,6 +64,30 @@ app.get("/blogs/:id", function(req, res){
 		}
 	});
 });
+
+//EDIT ROUTE
+app.get("/blogs/:id/edit", function(req, res){
+	Blog.findById(req.params.id, function(err, foundBlog){
+		if(err){
+			res.render("edit");
+		} else {
+			res.render("edit", {blog: foundBlog});
+		}
+	});
+});
+
+//UPDATE ROUTE
+app.put("/blogs/:id", function(req, res){
+	//(id, newData, callback)
+	Blog.findByIdAndUpdate(req.params.id, req.body.blog, function(err, updatedBlog){
+		if(err){
+			res.redirect("/blogs");
+		} else {
+			res.redirect("/blogs/" + req.params.id);
+		}
+	});
+});
+
 
 app.listen(3000, function(){
 	console.log("Server is running")
